f5ee3a37feb73fa5dbd78ea1cdfbacb886f5f089
calishaider <calishaider@gmail.com>
2026-03-12 17:41:03 -0700

    Add files via upload
    Added user prompt and switch case for day names.

--- dayOfWeek.cpp
+++ dayOfWeek.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    cout << "\nEnter Date (Day, Month, Year): ";
     int day, month, year;
     cin >> day >> month >> year;
  
@@ -22,7 +23,22 @@ int main() {
     int j = y / 100;
 
     int ans = (q + (13*(m + 1))/5 + k + k/4 + j/4 + 5*j ) % 7;
-    if (ans < 0) ans += 7;
-    cout << ans; 
+
+    if (ans < 0) // to get positive always
+        ans += 7;
+
+    cout << endl;
+    switch(ans)
+    {
+        case 0: cout<<"Saturday"; break;
+        case 1: cout<<"Sunday"; break;
+        case 2: cout<<"Monday"; break;
+        case 3: cout<<"Tuesday"; break;
+        case 4: cout<<"Wednesday"; break;
+        case 5: cout<<"Thursday"; break;
+        case 6: cout<<"Friday"; break;
+    }
+    cout <<endl <<endl;
+
     return 0;
 }
