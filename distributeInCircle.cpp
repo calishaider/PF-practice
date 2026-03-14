6ef2b18ba21987ee64551c5a74e696fc9af2c16d
calishaider <calishaider@gmail.com>
2026-03-14 09:04:23 -0700

    Add files via upload

---
+++ distributeInCircle.cpp
@@ -0,0 +1,60 @@
+// Problem Description:
+// A items are to be delivered in a circle of size B.
+// Find the position where the Ath item will be delivered if we start from a given position C.
+// NOTE: Items are distributed at adjacent positions starting from C.
+
+// Problem Constraints:
+// 1 <= A, B, C <= 108
+
+// Input Format:
+// First argument is an integer A.
+// Second argument is an integer B.
+// Third argument is an integer C.
+
+// Output Format:
+// Return an integer denoting the position where the Ath item will be delivered if we start from a given position C.
+
+// Example Input:
+// Input 1:
+//  A = 2
+//  B = 5
+//  C = 1
+// Input 2:
+//  A = 8
+//  B = 5
+//  C = 2
+
+// Example Output:
+// Output 1:
+//  2
+// Output 2:
+//  4
+
+// Example Explanation:
+// Explanation 1:
+//  The first item will be given to 1st position. Second (or last) item will be delivered to 2nd position
+// Explanation 2:
+//  The last item will be delivered to 4th position
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cout << "\nEnter total items to distribute: ";
+    cin >> A;
+    cout << "Enter number of positions arranged in a circle: ";
+    cin >> B;
+    cout << "Enter starting position: ";
+    cin >> C;
+
+    int ans = C + A - 1;
+    ans = ans % B; // for cyclic process after B comes 1
+
+    if(ans == 0)
+        ans = B;
+
+    cout << "\nPosition where the Ath item will be delivered: " << ans;
+    cout << endl << endl;    
+}
